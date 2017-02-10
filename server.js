8daaaeadd19f31ef1e9a5afe218cd8f0ff139a22
wujohns <wujohns@163.com>
2017-02-10 17:45:09 +0800

    parts of examples

--- server.js
+++ server.js
@@ -11,6 +11,24 @@ const app = express();
 const port = 80;
 
 app.use(express.static(path.join(__dirname, '')));
+app.use('/', (req, res) => {
+    res.end(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>BBPack Demos</title>
+        </head>
+        <body>
+            <h1>BBPack Demos</h1>
+            <h3><a href='demo1/index.html'>Demo1: Basic Usage</a></h3>
+            <h3><a href='demo2/index.html'>Demo2: Use Libs Pack Feature</a></h3>
+            <h3><a href='demo3/index.html'>Demo3: Sourcemap, Uglify And Watch Files' Change</a></h3>
+            <h3><a href='demo4/index.html'>Demo4: Custom Browserify's transforms or plugins</a></h3>
+            <h3><a href='demo5/index.html'>Demo5: Use With Browsersync (custome afterPipes)</a></h3>
+        </body>
+        </html>
+    `);
+});
 
 const server = http.createServer(app);
 server.on('error', (err) => {
